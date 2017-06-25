df3aaf9852fb7adfb0e0453c9f4a24b639d6df3b
Vadim Sokoltsov <vforvad@gmail.com>
2017-06-25 23:13:21 +0300

    load libraries and check data for R script

--- titanic/titanic_analysis.R
+++ titanic/titanic_analysis.R
@@ -1,1 +1,18 @@
-setwd("~.kaggle/titanic")
+setwd("~/kaggle/titanic")
+
+# Load packages
+library('ggplot2') # visualization
+library('ggthemes') # visualization
+library('scales') # visualization
+library('dplyr') # data manipulation
+library('mice') # imputation
+library('randomForest') # classification algorithm
+
+# Read and take a peek at the data
+train <- read.csv('train.csv', stringsAsFactors = F)
+test  <- read.csv('test.csv', stringsAsFactors = F)
+
+full  <- bind_rows(train, test) # bind training & test data
+
+# check data
+str(full)
